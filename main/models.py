62846e33a191960c0cd069eb0fd7882e84c60556
muskan5103 <muskanmahajan736@gmail.com>
2026-06-14 23:27:53 +0530

    Blog

--- main/models.py
+++ main/models.py
@@ -36,7 +36,7 @@ class Contact(models.Model):
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
-    image = models.ImageField(upload_to='blogs/')
+    image = models.ImageField(upload_to='projects/')
     short_description = models.CharField(max_length=300)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
